ea031832fbe0c3e7c82c8e2d6cddaa69b7d922fc
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-08 13:15:49 -0400

    name update

---
+++ exercises/1-38-1.js
@@ -0,0 +1,23 @@
+var customerId = "ID123456";
+console.log(customerId);
+
+var customerFirstName = "Yaritza";
+console.log(customerFirstName);
+
+var customerMiddleName = "null";
+
+var customerLastName = "Bustamante";
+console.log(customerLastName)
+
+var customerGender = "Female";
+console.log(customerGender)
+
+var customerDateOfBirth = "01/06/1999";
+console.log(customerDateOfBirth)
+
+var customerDriversLicenseNumber = "ID123456";
+console.log(customerDriversLicenseNumber)
+
+var customerPolicyNumber = "IZW123400";
+console.log(customerPolicyNumber)
+
