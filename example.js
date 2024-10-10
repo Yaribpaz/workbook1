36c03405f3d0e039aa2e85c98b75327559b4687e
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-10 13:33:39 -0400

    initial commit

---
+++ example.js
@@ -0,0 +1,13 @@
+"use strict"
+
+var number = 4;
+
+var condition = number > 5
+console.log(condition);
+
+
+if (condition) {
+    console.log("The number is greater than 5");
+}else{
+    console.log("The number is less than 5");
+}
