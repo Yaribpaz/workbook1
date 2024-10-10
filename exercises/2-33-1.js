91a9150c263f63a73df2ac493fdc4afa0ed688cd
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-10 13:36:19 -0400

    updated added comment

--- exercises/2-33-1.js
+++ exercises/2-33-1.js
@@ -1,12 +1,11 @@
-"use strict"
+"use strict";
 var a = "  101.1   ";
 a = parseInt(a);
 console.log(a);
 a = parseFloat(a);
-console.log(a)
+console.log(a);
 var a = Number(101.1);
 console.log(a);
-
 
 var b = "55";
 b = parseInt(b);
@@ -16,21 +15,18 @@ console.log(b);
 var b = Number(55);
 console.log(b);
 
-
 var c = "402 Stevens";
 c = parseInt(c);
 console.log(c);
-c = parseFloat(c)
+c = parseFloat(c);
 console.log(c);
 var c = Number(402);
 console.log(c);
 
-
-var d = "Number 5  ";
-d = parseInt(d)
-console.log(d);
-d = parseFloat(d);
-console.log(d);
-var d = Number(Number 5);
-console.log(d);
-
+// var d = "Number 5  ";
+// d = parseInt(d)
+// console.log(d);
+// d = parseFloat(d);
+// console.log(d);
+// var d = Number(Number 5);
+// console.log(d);
