c7d857801630ab3124060e12110f270d47f77ce8
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-10 13:33:32 -0400

    updated formula

--- exercises/3-7-1.js
+++ exercises/3-7-1.js
@@ -1,20 +1,21 @@
-"use strict"
-var payRate = 12.50;
-var hours = 20;
-var grossPay = 0;
+"use strict";
+// 1. Create variables for payRate and hoursWorked
+// 2. if the person works more than 40 hours
+// 3.Pay the overtime hours at 1.5 x the regular rate of hours
 
-grossPay = payRate * hours
-console.log(grossPay)
+var payRate1 = 12.5;
+var payRate2 = 25.0;
+var payRate3 = 17.3;
 
-var payRate3 = 17.30;
-var overtimeMultiplier = 1.5;
-var overtimePayRate = payRate3 * overtimeMultiplier * grossPay;
-var hours3 = 45;
-var grossPay3 = 0;
+var hoursWorked1 = 20;
+var hoursWorked2 = 40;
+var hoursWorked3 = 45;
 
-if(hours > 40){
-    grossPay3 = grossPay3 + overtimePayRate;
-    console.log(grossPay.toFixed(2))
-}else{
-    console.log("No overtime")
-}
+var totalPay1 = 0;
+
+// regularHours: 40
+// overtimeHours: 5
+// totalHours: 45
+// (40 * payRate) + ((hoursWorked - 40) * payRate * 1.5) - overtime
+// hoursWorked * payRate - regular time
+
