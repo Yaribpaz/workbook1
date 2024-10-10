a6a8c09c8d1965b899f9763903b7cba94bf5d519
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-10 13:33:45 -0400

    initial commit

---
+++ simple-example.js
@@ -0,0 +1,22 @@
+// var number = 6;
+
+// var remainder = number % 2;
+
+// var condition = remainder == 0
+// console.log(remainder);
+
+// if (condition){
+//     console.log("The number is even");
+
+// } else {
+//     console.log("The number is odd");
+// }
+
+var isLoggedIn = true; 
+
+if (isLoggedIn){
+    console.log("Welcome, user!");
+} else {
+    console.log("Please log in");
+}
+
