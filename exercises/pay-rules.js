20de4241c9cd1a10a202ba97f68a77026a7ed4fd
Yaritza Bustamante <yaritzab99@gmail.com>
2024-10-10 14:49:40 -0400

    success ! added if and else

--- exercises/pay-rules.js
+++ exercises/pay-rules.js
@@ -1,4 +1,51 @@
 "use strict"
-var regularPayRate = 12.50;
-var regularHours = 20;
-var regularPay = 250;
+// var regularPayRate = 12.50;
+// var regularHours = 20;
+// var regularPay = 250;
+
+// var payRate = 17.30;
+// var overtimePayRate = payRate * 1.5;
+// var regularPay = 0;
+// var overtimePay = 0;
+// var hoursWorked = 45;
+// var regularHours = 40;
+// var overtimeHours = 0;
+// var grossPay = 0;
+
+// overtimeHours = hoursWorked - regularHours;
+
+// console.log(overtimeHours)
+// if(hoursWorked <=40){
+//     grossPay = payRate * hoursWorked;
+// }
+
+// else{
+//     regularPay = regularHours * payRate
+//     overtimePay = overtimeHours * overtimePayRate
+//     grossPay = regularPay + overtimePay
+// }
+// console.log(grossPay);
+
+
+var payRate = 17.30;
+var overtimePayRate = payRate * 1.5;
+var regularPay = 0;
+var overtimePay = 0;
+var hoursWorked = 45;
+var regularHours = 40;
+var overtimeHours = 0;
+var grossPay = 0;
+
+overtimeHours = hoursWorked - regularHours;
+
+console.log(overtimeHours)
+if(hoursWorked <=40){
+    grossPay = payRate * hoursWorked;
+}
+
+else{
+    regularPay = regularHours * payRate
+    overtimePay = overtimeHours * overtimePayRate
+    grossPay = regularPay + overtimePay
+}
+console.log(grossPay);
